24e12f270c25dbf2ee072406f1d0b083f25933ea
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2024-03-11 09:32:35 +0100

    install

---
+++ lamp/dev/app/app/config/parameters.php
@@ -0,0 +1,64 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'bd',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'user',
+    'database_password' => 'abc123.',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'NG49tmYIwyOSKCMtyV9RatFMzXT9y1yRORYV5Lv0FbuFy6khb2LfTpJOSyoXUqZm',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2024-03-11',
+    'locale' => 'es-ES',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'AnaNxjekchRqQZ3EVKSDQDAXuAqQxQiPVqMMAxj1mfJFKtjUdZb5t8d7OT7DJ6No',
+    'cookie_iv' => 'Sg4Mf2hZCop6WjodTmGPZgoV4qGafsvH',
+    'new_cookie_key' => 'def000006bc3b876433c4fc8c00c7f3625902672084516c2c4b30de7d96d10c61c3cf0e4acf9a5d82fe60aa80be39754db2e95f35b9dc92d979722e127d5aec3ddc38d27',
+    'api_public_key' => '-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA9VlHlpEPgDJQ5xbRpm4D
+0ti9E1JW+qZ7v5lkQjfHSz54mLvoIl36Q6xu5k/APrm+mMaonI+p6Q9PUQwwIdTf
+YfxzLNIPGlHvtLW8lqMOqIo4INBI0FYxw/d4gdFRMDl2TBs2AZdxEOZKOBBuZ117
+3ou2xewvrcQctunurh3XJFaxbQiQsuVMIy0WXWlqHJ97OYTLFh479ffBSSfkF4h8
+pW1eripysgEOcEGjAwL2vOGjpmJVDisw0Biyr6XYmFTu5O1v1ZtjMRuKW8eh1T5W
+3WpBNDeicCSA/V3KrazLeb11K9vZga3D7CN3OxR41vWWNfKXi1XbIrTZXKMulhBD
+vwIDAQAB
+-----END PUBLIC KEY-----
+',
+    'api_private_key' => '-----BEGIN PRIVATE KEY-----
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQD1WUeWkQ+AMlDn
+FtGmbgPS2L0TUlb6pnu/mWRCN8dLPniYu+giXfpDrG7mT8A+ub6Yxqicj6npD09R
+DDAh1N9h/HMs0g8aUe+0tbyWow6oijgg0EjQVjHD93iB0VEwOXZMGzYBl3EQ5ko4
+EG5nXXvei7bF7C+txBy26e6uHdckVrFtCJCy5UwjLRZdaWocn3s5hMsWHjv198FJ
+J+QXiHylbV6uKnKyAQ5wQaMDAva84aOmYlUOKzDQGLKvpdiYVO7k7W/Vm2MxG4pb
+x6HVPlbdakE0N6JwJID9XcqtrMt5vXUr29mBrcPsI3c7FHjW9ZY18peLVdsitNlc
+oy6WEEO/AgMBAAECggEARllgqQAUET6cKv7Eg3OlHgZuhIVGEl29Ou15LXF/8AfM
+HYM3pu8KbPAa5mf0qCerSnQf8bBR2FU++W7EuFao7Jgt0Cq1EOdwk4DOv+SmNdZY
+DLScRDyKzXV+dL63iFvZmy3sON2bGmzl2BiCB5prA59M+4hjDXCfiSUffoZo4252
+fYOm6oe6wjh02YATQz9+UYh9DBusr1kqnmdgI7nQRsIyov/62cpSZct7YG8+Kmp4
+d2pvlKNvCba2hjqgGjo1a1uDGLbMlAJBzvsTTpw00IxusOaZORaYci8jDgEUhWbJ
+fj7DjxobxIIC/IoNvSkLx6yil6lxTLNHxfi5NG5q5QKBgQD6rSbysmFrLDFWc9zr
+4eKuekw7A/CbskvWUSsaTlnqro7LywwyhlT3B9g0P9T1qc/v2g8xaaaoLv7flv4G
+kRJFDSCA6/gnIxCpjhOHdt5qACeTtnkJn9ZdCOZRiwMAOWYveMiUjBOWYJDk6ZHp
+Mx7e7vQZgu5bWUhib/BHpPg7+wKBgQD6jymyydtiIjawfBEvo8TLspaMSgz5Xc9F
+PCTxJFCYEsbgr7ybyiQ7USUk5g9rNeUbw9a++t44Mi8wdqyEElvrOD8ryqO219vJ
+CN7eeobePj9Q+nsxzmhDLqnuIR19DDbkMq4bNoVwOqWk4tdvwIS5wrNX3QqON/fI
+SR5YgmIoDQKBgERCnlZ8SJbcg9rc1g3NhuKoHtX7LqvXE1ua4rfZGhcTzYKlGf8w
+ITbbhf5EW4NB6IORx41XDbDCi1jrTuUO4AC5oF4642oU3Favf5Xlwo7u19gbWj5K
+rh7La5xM1FZdSvwmUchRlIvgY9RR3g+4gfGG9CV2R93MJC/Z934q6AW9AoGBAK+E
+nCw/VSwgVXk53/wkV9ZtqyJpOT9yJKEWabZ2vvJYrPJ2iMQFTW40ALYhZwmuPmk9
+vfAY4pQxK3dimqUGC1fyFeaCVD7gZXkleqIU6OdEaaw7WxNfyQ/O34ENt4+Cf+1J
+R0eO4NjmbGTd9xa0oQVTy7gwApXvxOaLufM8mq/9AoGBANRhYN5EZULQI+9CCDun
+0Lm3HcPdmt0BmsI8fLJvZq5kXh/La5/saCC4Y07GVW8jQSGUQTmW89+GmhyPZqoT
+eK4ojKmn73iPP+qbpK8OwRKrPdAaza/1DW25iYYFOKS5/9rBwsM3nQdW8rFnUpXX
+qPkwB9zj2JEMpT5tP/eOzfFp
+-----END PRIVATE KEY-----
+',
+  ),
+);
